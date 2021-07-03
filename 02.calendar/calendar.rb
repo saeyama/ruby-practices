8a4a5180a65cfca8d67706a3648a1f8b731949fb
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-07-03 16:34:26 +0900

    calendarプログラムを修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -13,22 +13,18 @@ set_day = Date.new(params["y"].to_i,params["m"].to_i)
 firstday = Date.new(set_day.year, set_day.month, 1)
 lastday = Date.new(set_day.year, set_day.month, -1)
 
-#改行
-line = firstday.wday - 1
-
 #1行目/月と年
 puts set_day.strftime("%-m月 %Y").center(22)
 
 #2行目/曜日
 week = "日 月 火 水 木 金 土"
 print "#{week.rjust(14)}\n"
-#3行目
+
+#3行目/日にち
 print "   " * firstday.wday
-(firstday..lastday).each do |day|
-  line += 1
-  if line % 7 == 0
-    puts "\n"
-  end
+(firstday..lastday).each.with_index(firstday.wday - 1) do |day, index|
+  index += 1
+  puts "\n" if index % 7 == 0
   print day.strftime("%-d").rjust(3)
 end
 puts "\n"
