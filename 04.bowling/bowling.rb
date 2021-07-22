5c5a30021daa0b53587050f54f9a271d9ada79f9
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-07-22 22:13:22 +0900

    bowlingプログラムを修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -13,32 +13,26 @@ scores.each do |s|
     end
 end
 
-# 9フレーム目までになる要素を取得
-change_frames = shots[0..-4]
-
-first_frames = []
-loop do
-  first_frames <<
-    if change_frames[0] == 10
-      [change_frames.shift]
+frames = []
+10.times do |i|
+  frames <<
+    if i < 9
+      shots[0] == 10 ? [shots.shift] : shots.shift(2)
     else
-      change_frames.shift(2)
+      shots
     end
-  break if change_frames[0].nil?
 end
-
-point_frames = first_frames + [shots[-3..-1]]
 
 # 合計値
 point = 0
-point_frames.each_with_index do |frame, index|
+frames.each_with_index do |frame, index|
   point +=
-    if frame == [10] && point_frames[index + 1] == [10] && (index < 9)
-      20 + point_frames[index + 2][0]
+    if frame == [10] && frames[index + 1] == [10] && (index < 9)
+      20 + frames[index + 2][0]
     elsif frame == [10] && (index < 9)
-      10 + point_frames[index + 1][0] + point_frames[index + 1][1]
+      10 + frames[index + 1][0] + frames[index + 1][1]
     elsif frame.sum == 10 && (index < 9)
-      frame.sum + point_frames[index + 1][0]
+      frame.sum + frames[index + 1][0]
     else
       frame.sum
     end
