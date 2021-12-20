28cf1f8bb8121c0a5e177ea3cda4b18b05bbf169
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-12-20 20:34:38 +0900

    options.rb作成

---
+++ 08.ls_object/options.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class ListDirectory
+  def initialize(files)
+    @files = files
+  end
+
+  def list_directory
+    split_list_directory.transpose.each do |split_file|
+      split_file.each { |file| print file.to_s.ljust(25) }
+      print "\n"
+    end
+  end
+
+  private
+
+  COLUMNS_NUMBER = 3
+
+  def create_list_directory
+    column_files = []
+    columns_number = 3
+    @files.each_slice(COLUMNS_NUMBER) do |list|
+      column_files << list
+    end
+
+    column_files.map do |file|
+      file.values_at(0...columns_number)
+    end.flatten
+  end
+
+  def split_list_directory
+    split_files = []
+    create_list_directory.each_slice(create_list_directory.size / COLUMNS_NUMBER) do |list|
+      split_files << list
+    end
+    split_files
+  end
+end
