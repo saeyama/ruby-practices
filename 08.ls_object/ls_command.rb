dfe9a90a491e5f1ac0aae2278b1abca6346a0909
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-12-20 20:42:22 +0900

    ls_command.rb作成・rubocop通過確認

---
+++ 08.ls_object/ls_command.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'optparse'
+require_relative 'ls_command_format'
+
+ls_command = Command.new(ARGV.getopts('alr'))
+ls_command.options_l?
